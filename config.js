9318d693b1fb13c089ccf710ae471375eb0204c4
Irsan <irsantyohadi@gmail.com>
2022-03-23 00:00:14 +0700

    change config

---
+++ config.js
@@ -0,0 +1,4 @@
+export const nftaddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const activeChainId = 1337;
+export const activeChainName = 'local net';
+export const activeRpcURL = 'http://localhost:8545/';
