1d86515b7d44cc953685fbc43e420525437637f0
Irsan <irsantyohadi@gmail.com>
2022-03-23 09:34:10 +0700

    add minting function

--- config.js
+++ config.js
@@ -2,3 +2,5 @@ export const nftaddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 export const activeChainId = 1337;
 export const activeChainName = 'local net';
 export const activeRpcURL = 'http://localhost:8545/';
+export const maxMint = 10;
+export const mintPrice = 0.04;
--- projectx/pages/index.tsx
+++ projectx/pages/index.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 
 import NFT from '../../artifacts/contracts/ProjectXNFT.sol/ProjectX.json';
-import { nftaddress } from '../../config';
+import { nftaddress, maxMint, mintPrice } from '../../config';
 
 interface App {
   isConnect: boolean;
@@ -12,7 +12,8 @@ interface App {
 
 const Home: NextPage<App> = (props) => {
   const { isConnect, signer } = props;
-  const [mint, setMint] = useState(1);
+  const [mint, setMint] = useState(0);
+  const [amount, setAmount] = useState(0);
   const totalCollection = 5000;
 
   useEffect(() => {
@@ -27,6 +28,29 @@ const Home: NextPage<App> = (props) => {
     setMint(totalSupply.toNumber());
   };
 
+  const mintNFT = async () => {
+    const nftContract = new ethers.Contract(nftaddress, NFT.abi, signer);
+    try {
+      setAmount(0);
+      const mintTx = await nftContract.mint(amount, {
+        value: ethers.utils.parseEther((amount * mintPrice).toString()),
+      });
+      await mintTx.wait(1);
+      const totalSupply = await nftContract.totalSupply();
+      setMint(totalSupply.toNumber());
+    } catch (error) {
+      console.log(error, 'ini errornya');
+    }
+  };
+
+  const handleOnChage = (e: HTMLInputElement) => {
+    if (+e.value > maxMint) {
+      setAmount(maxMint);
+      return;
+    }
+    setAmount(+e.value);
+  };
+
   const renderCard = () => (
     <div className='flex  flex-col block p-6 rounded-lg shadow-lg bg-white max-w-sm'>
       <h5 className='text-center text-gray-900 text-xl leading-tight font-medium mb-2'>Current Mint</h5>
@@ -35,6 +59,11 @@ const Home: NextPage<App> = (props) => {
       </h5>
       <div className='flex self-center mb-2'>
         <button
+          onClick={() => {
+            if (amount > 0) {
+              setAmount(amount - 1);
+            }
+          }}
           type='button'
           className='text-center w-12 mr-3 py-2.5 text-black border-slate-600 border-2 font-bold text-xl leading-tight uppercase rounded shadow-md hover:shadow-lg transition duration-150 ease-in-out'
         >
@@ -42,12 +71,16 @@ const Home: NextPage<App> = (props) => {
         </button>
         <input
           type='text'
+          min={0}
+          max={maxMint}
+          value={amount}
+          onChange={(e) => handleOnChage(e.target)}
           className='
               form-control
               block
               w-12
               h-12
-              px-4
+              text-center
               py-2
               text-xl
               font-normal
@@ -63,16 +96,21 @@ const Home: NextPage<App> = (props) => {
         />
         <button
           type='button'
+          onClick={() => {
+            if (amount < maxMint) {
+              setAmount(amount + 1);
+            }
+          }}
           className='text-center w-12 ml-3 py-2.5 text-black border-slate-600 border-2 font-bold text-xl leading-tight uppercase rounded shadow-md hover:shadow-lg transition duration-150 ease-in-out'
         >
           +
         </button>
       </div>
       <button
-        disabled={!isConnect}
-        onClick={() => console.log('masuk')}
+        disabled={!isConnect || amount === 0}
+        onClick={() => mintNFT()}
         type='button'
-        className='inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
+        className='inline-block px-6 py-2.5 bg-blue-600 disabled:bg-gray-400 disabled:cursor-not-allowed cursor-pointer text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
       >
         Mint
       </button>
